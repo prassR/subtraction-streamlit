9c3b0a5bb01b980347828ce751fa7549329bc67f
prassR <92257326+prassR@users.noreply.github.com>
2022-12-06 06:44:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
 st.write("# My Subtraction Application")
 st.write("Subtraction of two numbers...")
 
-def user_input():
-  a = st.number_input("Enter first number...")
-  b = st.number_input("Enter second number...")
-  data = {"A":a, "B":b}
-  input = pd.DataFrame(data, index=[0])
-  return input
 
 #data = user_input();
 x = st.number_input("Enter first number...")
